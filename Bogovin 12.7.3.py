34ce479ffda1fea2f730f5e240e7b938fbc9db40
Vlad <116669526+VladTestQA@users.noreply.github.com>
2022-11-10 19:23:18 +0300

    Delete Bogovin 12.7.3.py

--- Bogovin 12.7.3.py
+++
@@ -1,9 +0,0 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = float(input('Сумма вклада, рублей:  '))
-
-deposit_rate = list(per_cent.values())
-deposit = [num * money / 100 for num in deposit_rate]
-deposit = [round(item, 2) for item in deposit]
-print("Ставки депозита по банкам: ", deposit_rate, '%')
-print("Годовой доход в каждом банке: ", deposit, 'рублей')
-print("Максимальный годовой доход: ", max(deposit), 'рублей')
